5c036eb5db6122216432146bfd4132bfc5886453
binsee <binsee@163.com>
2017-12-19 19:25:22 +0800

    refactor: 重构，接入新ipad协议服务

---
+++ example/demo.js
@@ -0,0 +1,141 @@
+'use strict'
+
+const WX = require('./wx')
+const fs = require('fs')
+
+const server = 'ws://127.0.0.1:8080/ws'
+const key = ''
+
+const wx = new WX(server)
+
+wx
+  .on('open', async () => {
+    let ret
+    ret = await wx.auth(key)
+      .catch(e => {
+        console.error('授权请求失败！', e.message)
+      })
+    if (!ret) {
+      console.warn('授权请求未成功！')
+      return
+    }
+    console.log('授权请求成功!')
+
+    ret = await wx.send('login')
+      .catch(e => {
+        console.error('登录请求失败！', e.message)
+      })
+    if (!ret || !ret.success) {
+      console.warn('请求登录未成功！ json:', ret)
+      return
+    }
+    console.log('请求登录成功, json: ', ret)
+
+    ret = await wx.send('getDeviceInfo')
+      .catch(e => {
+        console.error('获取设备参数失败！', e.message)
+      })
+    if (!ret || !ret.success) {
+      console.warn('获取设备参数未成功！ json:', ret)
+      return
+    }
+    console.log('获取设备参数成功, json: ', ret)
+  })
+  .on('qrcode', data => {
+    if (!data.QrCode) {
+      console.error('没有在数据中获得登陆二维码！')
+      return
+    }
+    fs.writeFileSync('./qrcode.jpg', Buffer.from(data.QrCode || '', 'base64'))
+    console.log('登陆二维码已经写入到 ./qrcode.jpg，请打开扫码登陆！')
+  })
+  .on('scan', (data, msg) => {
+    switch (data.Status) {
+      case 0:
+        console.log('等待扫码...')
+        break;
+      case 1:
+        console.log('已扫码，请在手机端确认登陆...')
+        break;
+      case 2:
+        switch (data.SubStatus) {
+          case 0:
+            console.log('扫码成功！登陆成功！')
+            break;
+          case 1:
+            console.log('扫码成功！登陆失败！')
+            break;
+          default:
+            console.log('扫码成功！子状态码:', data.SubStatus)
+            break;
+        }
+        break;
+      case 3:
+        console.log('二维码已过期！')
+        break;
+      case 4:
+        console.log('手机端已取消登陆！')
+        break;
+      default:
+        if (msg) {
+          console.warn('scan事件返回提示信息：', msg)
+        }
+        break;
+    }
+  })
+  .on('login', (data, msg) => {
+    console.log('微信账号登陆成功！', msg)
+  })
+  .on('logout', (data, msg) => {
+    console.log('微信账号已退出！', msg)
+  })
+  .on('loaded', (data, msg) => {
+    console.log('通讯录同步完毕！', msg)
+  })
+  .on('sns', (data, msg) => {
+    console.log('收到朋友圈事件！请查看朋友圈新消息哦！', msg)
+  })
+  .on('push', data => {
+    // 消息类型 data.MsgType
+    // 1  微信收到文字消息的推送，一般为微信服务器发过来，我们直接转发给你们的
+    // 2  好友信息推送，包含好友，群，公众号信息
+    // 3  收到图片消息
+    // 34  语音消息
+    // 35  用户头像buf
+    // 37  收到好友请求消息
+    // 42  名片消息
+    // 43  视频消息
+    // 47  表情消息
+    // 48  定位消息
+    // 49  APP消息(文件 或者 链接 H5)
+    // 50  语音通话
+    // 51  状态通知（如打开与好友/群的聊天界面）
+    // 52  语音通话通知
+    // 53  语音通话邀请
+    // 62  小视频
+    // 2000  转账消息
+    // 2001  收到红包消息
+    // 3000  群邀请
+    // 9999  系统通知
+    // 10000  微信通知信息. 微信群信息变更通知，多为群名修改，进群，离群信息，不包含群内聊天信息
+    // 10002  撤回消息
+    // --------------------------------
+    // 注意，如果是来自微信群的消息，data.Content字段中包含发言人的wxid及其发言内容，需要自行提取
+    // 各类复杂消息，data.Content中是xml格式的文本内容，需要自行从中提取各类数据。（如好友请求）
+    switch (data.MsgType) {
+      case 2:
+        console.log('收到推送联系人：', data)
+        break
+
+      default:
+        console.log('收到推送消息：', data)
+        break
+    }
+  })
+  .on('error', e => {
+    console.error('错误事件：', e)
+  })
+  .on('other', data => {
+    // 可以忽略此事件，正常情况下应该不会触发
+    console.warn('收到异常数据！', data)
+  })
---
+++ example/wx.js
@@ -0,0 +1,181 @@
+'use strict'
+
+const ws = require('ws')
+const UUID = require('uuid')
+const EventEmitter = require('events');
+
+const WxServer = 'ws://127.0.0.1:8080/ws'
+
+/**
+ * 微信ipad协议访问类
+ *
+ * @class Wx
+ * @extends {EventEmitter}
+ */
+class Wx extends EventEmitter {
+
+  /**
+   * Creates an instance of Wx.
+   * @param {any} [address] 远程服务器地址,如ws://127.0.0.1:8080/ws
+   * @memberof Wx
+   */
+  constructor(address = WxServer) {
+    super()
+    this._event = new EventEmitter()
+    this._timeout = 5000 // 向ws服务器提交指令后，返回结果的超时时间，单位毫秒
+    this.ws = new ws(address)
+    this._On()
+  }
+  _On() {
+    this.ws
+      .on('open', () => {
+        this.emit('open')
+      })
+      .on('close', () => {
+        this.emit('close')
+      })
+      .on('message', msg => {
+        let data
+        try {
+          if (typeof msg === 'string') {
+            data = JSON.parse(msg)
+          } else {
+            throw new Error('ws传输的数据不是字符串格式！')
+          }
+        } catch (e) {
+          this.emit('error', new Error('解析msg数据失败: ' + e.message))
+        }
+        this._onMsg(data)
+      })
+      .on('error', e => {
+        this.emit('error', e)
+      })
+  }
+
+  async _onMsg(data) {
+    if (data.cmdId) {
+      // 指令的执行结果
+      this._event.emit(data.cmdId, data)
+      return
+    }
+    let tmp
+    let ret
+    let content
+    switch (data.event) {
+      case 'cmdRet':
+        console.warn('捕捉到未包含cmdId的操作返回: ', data)
+        this.emit('error', new Error('捕捉到未包含cmdId的操作返回'))
+        break;
+      case 'error':
+        this.emit('error', new Error('服务器返回错误提示：' + data.msg))
+        break
+      case 'qrcode': // 微信扫码登陆，推送二维码
+      case 'scan': // 微信账号扫码事件
+      case 'login': // 微信账号登陆成功
+      case 'loaded': // 通讯录载入完毕
+      case 'logout': // 微信账号退出
+      case 'sns': // 朋友圈事件：新评论
+        this.emit(data.event, data.data || {}, data.msg)
+        break
+      case 'push':
+        if (!data.data || !Array.isArray(data.data) || data.data.length <= 0) {
+          this.emit('error', new Error('推送数据异常！'))
+          break
+        }
+        data.data.forEach(i => {
+          this.emit('push', i)
+        })
+        break
+      default:
+        this.emit('other', data)
+        break
+    }
+  }
+
+
+  _send(data, cb) {
+    if (!data.cmdId) {
+      data.cmdId = UUID.v1()
+    }
+    this._event.once(data.cmdId, ret => {
+      cb(null, ret)
+    })
+    this.ws.send(JSON.stringify(data), e => {
+      if (e) {
+        cb(new Error('发送失败: ' + e.message))
+      }
+    })
+  }
+
+  async _asyncSend(data, timeout = this._timeout) {
+    const tmp = UUID.v1()
+    console.time(tmp)
+    return await new Promise((res, rej) => {
+      let ok = false
+      setTimeout(() => {
+        if (ok) { return }
+        ok = true
+        console.timeEnd(tmp)
+        rej(new Error('请求超时！'))
+      }, timeout);
+      this._send(data, (err, ret) => {
+        if (ok) { return }
+        ok = true
+        console.timeEnd(tmp)
+        if (err) {
+          rej(err)
+        } else {
+          res(ret)
+        }
+      })
+    })
+  }
+
+  /**
+   *  发送指令，并返回执行结果
+   *
+   * @param {any} command 指令
+   * @param {any} data 指令附加数据
+   * @returns
+   * @memberof Wx
+   */
+
+  async send(command, data = null) {
+    if (!command) {
+      throw new Error('未指定操作指令！')
+    }
+    return await this._asyncSend({
+      code: command,
+      data,
+    })
+      .catch(e => {
+        throw e
+      })
+  }
+
+  /**
+   * 验证授权
+   *
+   * 需要注意：连接ws后，需要在10秒内验证授权，否则服务器端会关闭连接。
+   *
+   * @param {any} key 授权key
+   * @returns
+   * @memberof Wx
+   */
+
+  async auth(key) {
+    if (!key) {
+      throw new Error('请提供访问授权key！')
+    }
+    const ret = await this._asyncSend({
+      code: 'connect',
+      authKey: key,
+    })
+      .catch(e => {
+        throw e
+      })
+    return ret && ret.success
+  }
+}
+
+module.exports = Wx
